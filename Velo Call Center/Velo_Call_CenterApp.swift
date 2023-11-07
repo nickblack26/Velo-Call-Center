f16ea4bcdd524fdf7a70f5e326ee8bcc3163db70
Nick Black <nickblack@VELO-V201849.local>
2023-11-07 11:06:23 -0600

    Initial Commit

---
+++ Velo Call Center/Velo_Call_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Velo_Call_CenterApp.swift
+//  Velo Call Center
+//
+//  Created by Nick Black on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Velo_Call_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
